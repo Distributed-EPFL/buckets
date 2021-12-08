d76617c01ac01abff98fbc7d388638405daf6b2b
Matteo Monti <matteo.monti@epfl.ch>
2021-12-08 16:50:59 +0100

    Implement `apply_sparse_attached`

---
+++ src/apply_sparse_attached.rs
@@ -0,0 +1,84 @@
+use crate::{BucketZip, Split};
+
+use rayon::prelude::*;
+
+pub fn apply_sparse_attached<Z, A, I, K, R>(
+    mut buckets: Z,
+    attachment: A,
+    split: Split<I>,
+    kernel: K,
+) -> Vec<R>
+where
+    Z: BucketZip,
+    A: Sync + Copy,
+    I: 'static + Send,
+    K: 'static + Send + Sync + Copy + Fn(&mut Z::Zip, A, I) -> Option<R>,
+    R: 'static + Send,
+{
+    let zip = buckets.zip().into_par_iter();
+
+    let (splits, _) = split.explode();
+    let splits = splits.into_par_iter();
+
+    let (zip, filtered): (Vec<_>, Vec<_>) = zip
+        .zip(splits)
+        .map(|(mut inners, inputs)| {
+            let outputs = inputs
+                .into_iter()
+                .filter_map(|input| kernel(&mut inners, attachment, input))
+                .collect::<Vec<_>>();
+
+            (inners, outputs)
+        })
+        .unzip();
+
+    buckets.unzip(zip);
+
+    let mut sparse = Vec::new();
+
+    for mut outputs in filtered {
+        sparse.append(&mut outputs);
+    }
+
+    sparse
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::{apply, Buckets};
+
+    use std::collections::HashMap;
+
+    #[test]
+    fn filter_powers() {
+        let mut map = Buckets::<HashMap<u64, u64>>::new();
+        let keys = Split::<u64>::with_key(0..50000, |key| *key);
+
+        let keys = apply(&mut map, keys, |map, key| {
+            map.insert(key, key * key);
+            key
+        });
+
+        let mut powers = apply_sparse_attached(&mut map, 33, keys, |map, attachment, key| {
+            let value = *map.get(&key).unwrap();
+
+            if value.count_ones() == 1 {
+                Some(value + attachment)
+            } else {
+                None
+            }
+        });
+
+        powers.sort();
+
+        assert_eq!(
+            powers,
+            vec![
+                34, 37, 49, 97, 289, 1057, 4129, 16417, 65569, 262177, 1048609, 4194337, 16777249,
+                67108897, 268435489, 1073741857
+            ]
+        );
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod apply;
 mod apply_attached;
 mod apply_sparse;
+mod apply_sparse_attached;
 mod bucket_zip;
 mod buckets;
 mod key;
@@ -11,5 +12,6 @@ pub const BUCKETS: usize = 64;
 
 pub use crate::{
     apply::apply, apply_attached::apply_attached, apply_sparse::apply_sparse,
-    bucket_zip::BucketZip, buckets::Buckets, key::Key, split::Split, splittable::Splittable,
+    apply_sparse_attached::apply_sparse_attached, bucket_zip::BucketZip, buckets::Buckets,
+    key::Key, split::Split, splittable::Splittable,
 };
